7ed01c9909aa99714774098408f19e40b46bb104
st0n1e <cis.verbruggen@student.kdg.be>
2023-12-07 20:54:35 +0100

    Fixed colorshema

--- config.h
+++ config.h
@@ -8,13 +8,12 @@ static const char *fonts[] = {
 	"CaskaydiaCoveNerdFont:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-//static const char *colors[SchemeLast][2] = {
-//	/*     fg         bg       */
-//	[SchemeNorm] = { "#bbbbbb", "#222222" },
-//	[SchemeSel] = { "#eeeeee", "#005577" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
-//};
-#include "/home/st0n1e/.cache/wal/colors-wal-dwm.h"
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
